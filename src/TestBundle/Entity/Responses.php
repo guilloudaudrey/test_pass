d5d72a57a1e86305a8c0e184ea9290865fef7552
audrey <audrey.guilloud2201@gmail.com>
2018-03-18 21:02:54 +0100

    entité responses

---
+++ src/TestBundle/Entity/Responses.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace TestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Responses
+ *
+ * @ORM\Table(name="responses")
+ * @ORM\Entity(repositoryClass="TestBundle\Repository\ResponsesRepository")
+ */
+class Responses
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="string", length=255)
+     */
+    private $text;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return Responses
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+}
+
